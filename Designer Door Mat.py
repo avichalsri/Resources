e9f287a46be62835a017236d341b459e934b3959
yoursauce <91735492+yoursauce@users.noreply.github.com>
2021-10-01 22:50:58 +0530

    Create Designer Door Mat.py

---
+++ Designer Door Mat.py
@@ -0,0 +1,5 @@
+link to question [https://www.hackerrank.com/challenges/designer-door-mat/problem](https://www.hackerrank.com/challenges/designer-door-mat/problem)
+
+n, m = map(int,input().split())
+pattern = [('.|.'*(2*i + 1)).center(m, '-') for i in range(n//2)]
+print('\n'.join(pattern + ['WELCOME'.center(m, '-')] + pattern[::-1]))
