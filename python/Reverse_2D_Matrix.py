b1f6430c73a70f254c4fd96897f70e416b17c1c8
Harshithardd <79263383+Harshithardd@users.noreply.github.com>
2021-10-13 01:05:47 +0530

    Create Reverse_2D_Matrix.py
    This is a Program to Reverse a 2D Matrix with the help of inbuilt function.

---
+++ python/Reverse_2D_Matrix.py
@@ -0,0 +1,9 @@
+#this is a Program to Reverse a 2D Matrix with the help of inbuilt function
+
+a = [[1,2,3],[4,5,6],[7,8,9]]
+b = []
+for i in a:
+    d = []
+    i.reverse()
+    b.append(i)
+print(b)
