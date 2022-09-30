e361f4da58601cb6f162030de23ddd84c537b330
Vikash Sangai <vikash36905@gmail.com>
2022-09-30 21:38:37 +0530

    Added cpp code to generate all subsets

---
+++ cpp/generateallsubsets.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> subsets;
+//Recursive fucntion to generate all subsets. Time complexity: 2^n(2 to the power n)
+void gensubset(vector<int> &v, int i, vector<int> &temp)
+{
+    if(i==v.size())
+    {
+        subsets.push_back(temp);
+        return;
+    }
+
+    //For every element, we can either take the element in the subset, or ignore it.
+    gensubset(v,i+1, temp);
+    temp.push_back(v[i]);
+    gensubset(v,i+1, temp);
+    temp.pop_back();
+}
+
+int main(){
+    //Taking input of size of array
+    int n;
+    cin>>n;
+    vector<int> v(n);
+    //Taking input of array
+    for(int i=0;i<n;i++)
+    {
+        cin>>v[i];
+    }
+    vector<int> temp;
+    gensubset(v, 0, temp);//calling recursive function to generate all subsets
+    //Printing all subsets
+    for(int i=0;i<subsets.size();i++)
+    {
+        for(int j=0;j<subsets[i].size();j++)
+        {
+            cout<<subsets[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
