7a7ccd9409cc9328bbfcb6454a32ef62d61de746
bilwa496 <66560100+bilwa496@users.noreply.github.com>
2022-10-01 18:41:43 +0530

    Updated the code with problem statement & example
    Updated the code with problem statement & example in detail

--- cpp/kmp_algorithm.cpp
+++ cpp/kmp_algorithm.cpp
@@ -1,4 +1,28 @@
 // C++ program for implementation of KMP pattern searching algorithm
+
+/*
+Problem Statement-
+Given a text txt[0..n-1] and a pattern pat[0..m-1], 
+write a function search(char pat[], char txt[]) that 
+prints all occurrences of pat[] in txt[].
+
+*/
+
+/*
+Examples-
+
+Input:  txt[] = "THIS IS A TEST TEXT"
+        pat[] = "TEST"
+Output: Pattern found at index 10
+
+Input:  txt[] =  "AABAACAADAABAABA"
+        pat[] =  "AABA"
+Output: Pattern found at index 0
+        Pattern found at index 9
+        Pattern found at index 12
+	
+*/
+
 #include <bits/stdc++.h>
 
 void computeLPSArray(char* pat, int M, int* lps);
