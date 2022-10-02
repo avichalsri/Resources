b486212c9806ff94ecb863f9543cc50919dd087c
Akshit Parmar <72138022+Akshitparmar@users.noreply.github.com>
2022-10-02 17:09:41 +0530

    Update eliminate duplicate nodes in linked list.cpp

--- cpp/eliminate duplicate nodes in linked list.cpp
+++ cpp/eliminate duplicate nodes in linked list.cpp
@@ -1,3 +1,16 @@
+/*
+QUESTION
+Eliminate duplicates from LL
+Level EASY
+Given a sorted linked list (elements are sorted in ascending order). Eliminate duplicates from the given LL, such that output LL contains only unique elements.
+You don't need to print the elements, just remove duplicates and return the head of updated LL.
+Input format : Linked list elements (separated by space and terminated by -1)
+Sample Input 1 :
+1 2 3 3 3 4 4 5 5 5 7 -1
+Sample Output 1 :
+1 2 3 4 5 7
+*/
+
 #include <iostream>
 using namespace std;
 
