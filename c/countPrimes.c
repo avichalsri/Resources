8b199f22fe6543838cb3365bf03e6290b121e345
Gulshan <61077022+Gulshan-11@users.noreply.github.com>
2022-10-03 12:35:35 +0530

    Updated file
    Added problem statement and example output

--- c/countPrimes.c
+++ c/countPrimes.c
@@ -1,3 +1,6 @@
+//Problem staement
+/*Write a code to find the number of prime numbers from 0 to given input range using sieve method */
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -7,7 +10,7 @@ int countprimes(int);
 int main()
 {
     int n;
-    printf("enter number: ");
+    printf("enter number:");
     scanf("%d", &n);
     printf("number primes less then %d are: %d ", n,countprimes(n));
     return 0;
@@ -28,3 +31,10 @@ int countprimes(int n) {
     free(primes);
     return count;
 }
+
+
+//Output
+/*
+enter number: 100
+number primes less than 100 are: 25
+*/
